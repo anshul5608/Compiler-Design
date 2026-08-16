381ee7ebecd512d9f001a380cbdae5a7aa006684
1zanami <anshulram5608@gmail.com>
2026-08-16 14:57:06 +0530

    Update test7_system_misc.c

--- lexical-analyzer/test/test7_system_misc.c
+++ lexical-analyzer/test/test7_system_misc.c
@@ -2,19 +2,16 @@
 
 int multiply(int a, int b) { return a * b; }
 
-// Command line input
 int main(int argc, char *argv[]) {
-    static int runCount = 0; // Static keyword
+    static int runCount = 0; 
 
-    // Function pointer
     int (*op)(int, int) = &multiply;
 
-    switch (argc) { // Switch cases
+    switch (argc) {
         case 1: break;
         default: break;
     }
 
-    // File manipulation
     FILE *fp = fopen("output.txt", "w");
     if (fp != NULL) {
         fclose(fp);
